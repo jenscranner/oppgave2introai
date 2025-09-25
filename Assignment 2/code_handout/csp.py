579f17cf8b0d52ce9f587211eff4f4e3717e2854
Jens Cranner <59847467+jenscranner@users.noreply.github.com>
2025-09-25 12:44:35 +0200

    Update csp.py

--- Assignment 2/code_handout/csp.py
+++ Assignment 2/code_handout/csp.py
@@ -63,12 +63,66 @@ class CSP:
         None | dict[str, Any]
             A solution if any exists, otherwise None
         """
+
+        def pair_allowed(v1: str, a1: int, v2: str, a2: int) -> bool: #Checks the binary_constraints in both directions. Kinda same as handout
+            if (v1, v2) in self.binary_constraints and (a1, a2) not in self.binary_constraints[(v1, v2)]:                 
+                #If they have an edge and values are not violating
+                return False
+            if (v2, v1) in self.binary_constraints and (a2, a1) not in self.binary_constraints[(v2, v1)]:
+                #Just inverse
+                return False
+            return True
+
+        def select_empty_cell(assignment: dict[str, set]): 
+            #Selects an empty cell by checking if it has a domain greater than one
+            #None if all domains have an assigned value
+            for var in self.variables:
+                if len(assignment[var]) > 1:
+                    return var
+            return None
+
+        def check_constraints(assignment: dict[str, set], var: str, val: int) -> bool:
+            for other in self.variables: #Others, i.e not itself
+                if other == var: #Dont check against itself
+                    continue
+                dom2 = assignment[other]
+                if len(dom2) == 1: #Only check for singletons 
+                    val2 = next(iter(dom2)) #Get that singleton value
+                    if not pair_allowed(var, val, other, val2): #Checks of pair violates binary constraints (for every pair)
+                        return False
+            return True
+        
+
+        def copy_domains(assignment: dict[str, Any]) -> dict[str, set]:
+            return {k: set(v) for k, v in assignment.items()} #Copies domains so we can undo
+
+        def solved (assignment: dict[str, Any]) -> bool:
+            return all(len(s) == 1 for s in assignment.values())
+
         def backtrack(assignment: dict[str, Any]):
-            # YOUR CODE HERE (and remove the assertion below)
-            assert False, "Not implemented"
+            if solved(assignment): #If domains are all singletons we have a complete solution
+                return {k: next(iter(v)) for k, v in assignment.items()}
 
-        return backtrack({})
+            empty_cell = select_empty_cell(assignment) #Finds empty_cell
+            if empty_cell is None:
+                return {k: next(iter(v)) for k, v in assignment.items()} #If we have no empty cells, we also have a solution
 
+
+            for tentative in sorted(self.domains[empty_cell]): #Tries values from the domain of the  empty cell
+
+                if not check_constraints(assignment, empty_cell, tentative):
+                    continue
+
+                assignment2 = copy_domains(assignment) #Copy domains
+                assignment2[empty_cell] = {tentative} #Set tentative assignment the empty cell
+
+                rec = backtrack(assignment2) #recurse
+                if rec is not None:
+                    return rec
+
+
+        start = copy_domains(self.domains)
+        return backtrack(start)
 
 def alldiff(variables: list[str]) -> list[tuple[str, str]]:
     """Returns a list of edges interconnecting all of the input variables
